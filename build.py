a3169d7e6d435a371a0992006589aeef0801cf7b
Miguel <miguel@bisect.pt>
2018-07-13 12:22:42 +0100

    Some fixes

--- build.py
+++ build.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bincrafters import build_template_default
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-
-    builder = build_template_default.get_builder()
+    builder = ConanMultiPackager(archs=["x86_64"], args="--build missing")
+    builder.add_common_builds()
     builder.run()
